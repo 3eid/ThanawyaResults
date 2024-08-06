0ee1d3775c8f2347e671c66be694fcd14da786ed
Mohamed Eid <66387447+3eid@users.noreply.github.com>
2024-08-06 12:12:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Read data from JSON file
-const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8'));
-
+let data;
+try {
+    data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8'));
+} catch (err) {
+    console.error('Error parsing JSON file:', err);
+    process.exit(1); // Exit the process if JSON parsing fails
+}
 // Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
